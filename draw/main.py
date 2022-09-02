7c1c99aec4803e2f175f757758f345ad11095fdf
lucheng <lucheng66@outlook.com>
2022-09-02 16:40:21 +0800

    first commit

---
+++ draw/main.py
@@ -0,0 +1,22 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import math
+
+a = np.arange(9)
+print('a = ', a)
+print('slice ', a[-6:-2])
+
+# x = np.arange(0, math.pi * 2, 0.05)
+# y = np.sin(x)
+# y1 = np.cos(x)
+#
+# fig = plt.figure()
+#
+# ax = fig.add_axes([0.15, 0.15, 0.75, 0.75])
+#
+# ax.plot(x, y, 'y')
+# ax.plot(x, y1, 'r')
+# ax.set_title("wave")
+# ax.set_xlabel('angle')
+# ax.legend(labels=('sin', 'cos'), loc='best')
+# plt.show()

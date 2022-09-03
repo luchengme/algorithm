d0b4822bbf1106feefe0ab9a5e5e21ccfaec8265
lucheng <lucheng66@outlook.com>
2022-09-03 20:41:22 +0800

    null

--- chapter01/question1.py
+++ chapter01/question1.py
@@ -5,6 +5,7 @@
 
 # 欧几里得算法
 # 定理：两个整数的最大公约数等于其中较小的那个数和两数相除余数的最大公约数
+# 时间复杂度O(log n)
 def f1(a, b):
     mod = a % b
     while mod != 0:
@@ -15,6 +16,7 @@ def f1(a, b):
 
 
 # 暴力循环求解
+# 时间复杂度O(n)
 def f2(a, b):
     s = a if a < b else b
     while s > 0:
@@ -26,11 +28,27 @@ def f2(a, b):
 # 更相减损术
 # 1、先判断两个数的大小，如果两数相等，则这个数本身就是就是它们的最大公约数。
 # 2、如果不相等，则用大数减去小数，然后用这个较小数与它们相减的结果相比较，如果相等，则这个差就是它们的最大公约数，而如果不相等，则继续执行操作2。
+# 时间复杂度O(n)
 def f3(a, b):
-    if a == b:
-        return a
-    s = a if a < b else b
-    gap = abs(a - b)
-    f3(s, gap)
-    return gap
+    while a != b:
+        if a > b:
+            a -= b
+        else:
+            b -= a
+    return a
+
+
+# from random import randint
+#
+# res1 = []
+# res2 = []
+# res3 = []
+# for i in range(1000):
+#     a = randint(1, 1000)
+#     b = randint(1, 1000)
+#     res1.append(f1(a, b))
+#     res2.append(f2(a, b))
+#     res3.append(f3(a, b))
+#
+# print(res1 == res2 == res3)
 
